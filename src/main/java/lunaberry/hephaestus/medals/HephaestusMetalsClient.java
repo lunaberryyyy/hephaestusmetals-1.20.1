a774f1424612c48989e510d9945553ac702d3a20
lunaberryyyy <riverpalasky@gmail.com>
2024-09-03 00:20:00 -0400

    Initial Commit

---
+++ src/main/java/lunaberry/hephaestus/medals/HephaestusMetalsClient.java
@@ -0,0 +1,12 @@
+package lunaberry.hephaestus.medals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+public class HephaestusMetalsClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/lunaberry/hephaestus/medals/HephaestusMetalsDataGenerator.java
@@ -0,0 +1,11 @@
+package lunaberry.hephaestus.medals;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HephaestusMetalsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
